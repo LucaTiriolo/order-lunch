01a2275265bb524846a728858a685cd73ed3f846
LucaTiriolo <luca.tiriolo@gmail.com>
2025-06-07 09:13:29 +0200

    First commit. Maven Project

---
+++ src/main/java/it/katana/order_lunch/OrderLunchApplication.java
@@ -0,0 +1,13 @@
+package it.katana.order_lunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderLunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/katana/order_lunch/OrderLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package it.katana.order_lunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderLunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
